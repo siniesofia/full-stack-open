fbd3fedcde18d0184cb207ff11996980c00ef0c8
Sini <siniesofia@protonmail.com>
2021-10-06 15:20:26 +0300

    commit

--- exercises/part1/kurssitiedot/src/App.js
+++ exercises/part1/kurssitiedot/src/App.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
+const Header = ( {course} ) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ( {part, exercises } ) => {
+  return (
+    <p>{part} {exercises} </p>
+  )
+}
+
+const Total = ( { exercises } ) => {
+  return (
+    <p>Number of exercises {exercises}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,15 +29,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>{part2} {exercises2}
-      </p>
-      <p>{part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
