28b9dca6ec73febb5276969c659b2b08e5fdcca9
Sini <siniesofia@protonmail.com>
2021-10-06 15:29:24 +0300

    step2

--- exercises/part1/kurssitiedot/src/App.js
+++ exercises/part1/kurssitiedot/src/App.js
@@ -6,9 +6,19 @@ const Header = ( {course} ) => {
   )
 }
 
-const Content = ( {part, exercises } ) => {
+const Content = ( {parts, exercises } ) => {
   return (
-    <p>{part} {exercises} </p>
+    <>
+      <Part part={parts[0]} exercises={exercises[0]} />
+      <Part part={parts[1]} exercises={exercises[1]} />
+      <Part part={parts[2]} exercises={exercises[2]} />
+    </>
+  )
+}
+
+const Part = ( {part, exercises} ) => {
+  return (
+    <p>{part} {exercises}</p>
   )
 }
 
@@ -30,9 +40,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
