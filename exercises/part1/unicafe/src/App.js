9be805f2335f8652966fa0e1190bc5ad77076089
Sini <siniesofia@protonmail.com>
2021-10-12 17:31:32 +0300

    tehtävä 1.10

--- exercises/part1/unicafe/src/App.js
+++ exercises/part1/unicafe/src/App.js
@@ -6,19 +6,36 @@ const Button = ({ handleClick, teksti }) => {
   )
 }
 
-const Statistics = ({ good, neutral, bad }) => {
+const StatisticLine = ({ text, value }) => {
   return (
     <div>
-      <h1>statistics</h1>
-      <p>{good}</p>
-      <p>{neutral}</p>
-      <p>{bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good*1+neutral*0+bad*-1)/(good+neutral+bad)}</p>
-      <p>positive {(good*1/(good+neutral+bad))*100} %</p>
+      {text} {value}
     </div>
   )
 }
+
+const Statistics = ({ good, neutral, bad }) => {
+  const yhteensa = good+neutral+bad
+  if (yhteensa === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    ) 
+  }
+    return (
+      <>
+        <StatisticLine text="good" value={good}></StatisticLine>
+        <StatisticLine text="neutral" value={neutral}></StatisticLine>
+        <StatisticLine text="bad" value={bad}></StatisticLine>
+        <StatisticLine text="all" value={good + neutral + bad}></StatisticLine>
+        <StatisticLine text="average" value={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}></StatisticLine>
+        <StatisticLine text="positive" value={(good*1/(good+neutral+bad))*100 + " %"}></StatisticLine>
+      </>
+    )
+  }
+  
+
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -42,6 +59,7 @@ const App = () => {
       <Button handleClick={handleGoodClick} teksti="good"></Button>
       <Button handleClick={handleNeutralClick} teksti="neutral"></Button>
       <Button handleClick={handleBadClick} teksti="bad"></Button>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} 
       ></Statistics>
   </div>
