7aae130d097902b396d6dedcc9f49958179161ba
Sini <siniesofia@protonmail.com>
2021-10-13 15:37:14 +0300

    alkujutut

--- exercises/part2/kurssitiedot2/src/App.js
+++ exercises/part2/kurssitiedot2/src/App.js
@@ -1,7 +1,72 @@
 import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello world lo23</p>
-  </div>
-)
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map(part => <p>{part.name} {part.exercises}</p>)}
+    </div>
+  )
+}
+
+const Header = ( {course} ) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ( {parts, exercises } ) => {
+  return (
+    <>
+      <Part part={parts[0]} exercises={exercises[0]} />
+      <Part part={parts[1]} exercises={exercises[1]} />
+      <Part part={parts[2]} exercises={exercises[2]} />
+    </>
+  )
+}
+
+const Part = ( {part, exercises} ) => {
+  return (
+    <p>{part} {exercises}</p>
+  )
+}
+
+const Total = ( { exercises } ) => {
+  return (
+    <p>Number of exercises {exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course= {course}></Course>
+    </div>
+  )
+
+
+}
+
 export default App
