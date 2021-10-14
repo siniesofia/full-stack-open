d0bd3c52ec066c318df7a2747b29cc2fbf7dfc16
Sini <siniesofia@protonmail.com>
2021-10-14 14:14:22 +0300

    eka tehtävä

--- exercises/part2/puhelinluettelo/src/App.js
+++ exercises/part2/puhelinluettelo/src/App.js
@@ -1,7 +1,48 @@
-import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello world lo23</p>
-  </div>
+import React, { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Amira Hellas' },
+    { name: 'Elli Ailas'}
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('event', event.target)
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <Person person={person}></Person>)}
+    </div>
 )
+}
 export default App
