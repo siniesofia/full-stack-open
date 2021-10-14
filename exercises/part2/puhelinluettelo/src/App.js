f03167e0666ae74dc256e383ba6c09b2c48bfd8b
Sini <siniesofia@protonmail.com>
2021-10-14 15:26:10 +0300

    osin toimii

--- exercises/part2/puhelinluettelo/src/App.js
+++ exercises/part2/puhelinluettelo/src/App.js
@@ -6,22 +6,51 @@ const Person = ({ person }) => {
   )
 }
 
+const checkIfNameExists = (persons, newName) => {
+  console.log("newName",newName)
+  console.log('tulee ylös')
+  console.log('persons', persons)
+  console.log('persons.length', persons.persons.length)
+
+  for (let i =0; i < persons.persons.length; i++) {
+    console.log('tulee looppin')
+    console.log('persons.persons[i].name', persons.persons[i].name)
+    console.log('newNamealla', newName.newName)
+    if (persons.persons[i].name === newName.newName) {
+      console.log('tulee tänne')
+      return false
+    }
+  }
+  return true
+
+}
+
+
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Amira Hellas' },
     { name: 'Elli Ailas'}
   ])
+
   const [ newName, setNewName ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    console.log('event', event.target)
-    const nameObject = {
-      name: newName,
+    if (checkIfNameExists({persons}, {newName})) {
+      console.log('tulee alle')
+      const nameObject = {
+        name: newName,
+        number: "000",
+      }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+    } else {
+      setNewName('')
     }
-    setPersons(persons.concat(nameObject))
-    setNewName('')
+
   }
+
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
