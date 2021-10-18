240ed0280eadb75852b65e3d23e27906c7fe9992
Sini <siniesofia@protonmail.com>
2021-10-18 15:51:48 +0300

    alku

--- exercises/part2/puhelinluettelo/src/App.js
+++ exercises/part2/puhelinluettelo/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import CheckForName from './components/CheckForName'
 
-
 const App = () => {
+  
   const [ persons, setPersons ] = useState([
     { name: 'Amira Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
-
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
@@ -45,10 +45,8 @@ const App = () => {
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
-
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
@@ -77,7 +75,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons persons={persons} newFilter={newFilter}></Persons>
     </div>
-    
 )
 }
+
 export default App
