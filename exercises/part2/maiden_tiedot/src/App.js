5b72142f1fd8a35be0c55d4300e88fc5080a5fd8
Sini <siniesofia@protonmail.com>
2021-10-19 13:44:54 +0300

    filtteröinti toimii

--- exercises/part2/maiden_tiedot/src/App.js
+++ exercises/part2/maiden_tiedot/src/App.js
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({ country }) => {
-  console.log('country', country)
+const Countries = ({ countries, filter }) => {
+  console.log('filter.length', filter.length)
+  if (countries.filter(country => country.name.common.includes(filter)).length > 10) {
+    return (
+      <div>Too many matcches, specify another filter</div>
+    )
+  } else {
+    return (
+      <div>
+        {countries.filter(country => country.name.common.toLowerCase().includes(filter))
+        .map(country => <CountryList key={country.name.common} country={country}></CountryList>)}
+      </div>
+    )
+  }
 
+}
+
+const CountryList = ({ country }) => {
   return (
     <div>
       {country.name.common}
@@ -12,24 +27,37 @@ const Country = ({ country }) => {
 }
 
 const App = () => {
-    const [ countries, setCountries] = useState([])
+  const [ countries, setCountries] = useState([])
+  const [ filter, setFilter ] = useState('')
 
-    useEffect(() => {
-      console.log('effect')
-      axios
-        .get('https://restcountries.com/v3/all')
-        .then(response => {
-          console.log('promise fullfilled')
-          setCountries(response.data)
-        })
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.com/v3/all')
+      .then(response => {
+        console.log('promise fullfilled')
+        setCountries(response.data)
+      })
 
-    }, [])
+  }, [])
 
-    console.log('render')
+  console.log('render')
+  
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   return (
     <div>
-      {countries.map(country => <Country country={country}></Country>)}
+      <form>
+        <div>
+          find countries
+          <input 
+          value={filter}
+          onChange={handleFilterChange}/>
+        </div>
+      </form>
+      <Countries countries={countries} filter={filter}></Countries>
     </div>
   )
 }
