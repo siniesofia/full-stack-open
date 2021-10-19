b1369059df00d6710fbb96e3dba1f4b3113d3166
Sini <siniesofia@protonmail.com>
2021-10-19 13:18:02 +0300

    fetchaustoimii

--- exercises/part2/maiden_tiedot/src/App.js
+++ exercises/part2/maiden_tiedot/src/App.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = () => {
+const Country = ({ country }) => {
+  console.log('country', country)
+
   return (
     <div>
-      <p>Hello world lo23</p>
+      {country.name.common}
+    </div>
+  )
+}
+
+const App = () => {
+    const [ countries, setCountries] = useState([])
+
+    useEffect(() => {
+      console.log('effect')
+      axios
+        .get('https://restcountries.com/v3/all')
+        .then(response => {
+          console.log('promise fullfilled')
+          setCountries(response.data)
+        })
+
+    }, [])
+
+    console.log('render')
+
+  return (
+    <div>
+      {countries.map(country => <Country country={country}></Country>)}
     </div>
   )
 }
