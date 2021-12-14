b0a7382210324f6e2e361c3338136b1e4e1f798d
Sini <siniesofia@protonmail.com>
2021-12-14 15:34:56 +0200

    tehtävät 5.6 ja 5.7 tehty

--- exercises/part5/bloglist-frontend/src/components/Blog.js
+++ exercises/part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,28 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import ToggleBlogs from './ToggleBlogs'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      {<ToggleBlogs buttonLabel='view'>
+        {blog.url}
+        <br></br>
+        likes {blog.likes} <button>like</button>
+        <br></br>
+        {blog.author} 
+        </ToggleBlogs>}
+      <br></br>
+    </div>  
+  )
+
+}
 
 export default Blog
